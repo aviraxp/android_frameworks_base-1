d031c68aaae1430ff3bb47b10a08176cf625bfab
Wang Han <416810799@qq.com>
2016-11-15 16:28:30 +0800

    Make Xposed modules from any paths be injected into Zygote

--- core/jni/fd_utils-inl.h
+++ core/jni/fd_utils-inl.h
@@ -264,10 +264,7 @@ class FileDescriptorInfo {
       // Xposed-powered Zygote might read from extensions other than .apk
       // so skip extension check
       ALOGW("Xposed detected, loosening up Zygote fd check!");
-      static const std::string kDataAppPrefix = "/data/app/";
-      if (path.compare(0, kDataAppPrefix.size(), kDataAppPrefix) == 0) {
-        return true;
-      }
+      return true;
     }
 
     return false;
